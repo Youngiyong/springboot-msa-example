312e3cc5f1e2be37457f6853086491dadf8e0ae3
Youngiyong <youn9354@naver.com>
2021-11-12 11:47:57 +0900

    zuul test

--- zuul-gateway/src/main/java/com/zuul/gateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/zuul/gateway/ZuulGatewayApplication.java
@@ -1,9 +1,14 @@
 package com.zuul.gateway;
 
+import com.zuul.gateway.filters.GatewayErrorFilter;
+import com.zuul.gateway.filters.GatewayPostFilter;
+import com.zuul.gateway.filters.GatewayPreFilter;
+import com.zuul.gateway.filters.GatewayRouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
@@ -14,4 +19,23 @@ public class ZuulGatewayApplication {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
 
+	@Bean
+	public GatewayPreFilter preFilter() {
+		return new GatewayPreFilter();
+	}
+
+	@Bean
+	public GatewayPostFilter postFilter() {
+		return new GatewayPostFilter();
+	}
+
+	@Bean
+	public GatewayRouteFilter routeFilter() {
+		return new GatewayRouteFilter();
+	}
+
+	@Bean
+	public GatewayErrorFilter errorFilter() {
+		return new GatewayErrorFilter();
+	}
 }
---
+++ zuul-gateway/src/main/java/com/zuul/gateway/filters/GatewayRouteFilter.java
@@ -0,0 +1,31 @@
+package com.zuul.gateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+@Slf4j
+public class GatewayRouteFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return FilterConstants.ROUTE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        log.info("Using Route Filter");
+        return null;
+    }
+}
+
